0b53c5a4a43f7c5626e2caf5b58a6552813bffd9
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-11-24 03:20:26 +0100

    feat: display user's email and role in profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,10 +1,35 @@
-
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+import { getUser } from '../api/usersApi';
+import { User } from '../types';
 
 const ProfilePage = () => {
-    return (
-      <>Profile Page</>
-    );
-  };
-  
-  export default ProfilePage;
-  
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const auth = getAuth();
+      if (!auth.currentUser) return;
+      const userData = await getUser(auth.currentUser.uid);
+      setUser(userData);
+      
+      setIsLoading(false);
+    };
+
+    fetchUserData();
+  }, []);
+
+  return (
+    <>
+      {isLoading ? <>Loading user data...</> : (
+        <ul>
+          {user ? <li>{user.email}</li> : null}
+          {user?.isAdmin ? <li>Admin</li> : null}
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default ProfilePage;
