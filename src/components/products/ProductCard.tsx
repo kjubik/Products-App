bb959fd904be9e040db445cd0d7e5d1f6aee309a
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-11-25 19:48:45 +0100

    feat: display all products from DB on ProductsPage

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,12 +1,22 @@
+import { Product } from "src/types";
+
+interface ProductCardProps {
+    product: Product;
+}
+
+const ProductsCard = (props: ProductCardProps) => {
 
 
-const ProductsCard = () => {
     return (
-    <>
-        Products Card
-    </>
+        <>
+            <div>
+                <img src={props.product.imageUrl} alt="product image" className="max-w-sm" />
+                <h4>{props.product.title}</h4>
+                <p>{props.product.description}</p>
+            </div>
+        </>
     );
-  };
-  
-  export default ProductsCard;
+};
+
+export default ProductsCard;
   
--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -1,9 +1,33 @@
-
+import { useState, useEffect } from "react";
+import ProductCard from "./ProductCard";
+import { getProducts } from "src/api/productsApi";
+import { Product } from "src/types";
 
 const ProductsList = () => {
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        const getProductsFromFirestore = async () => {
+            const productsFromFirestore = await getProducts();
+            setProducts(productsFromFirestore);
+        }
+
+        getProductsFromFirestore();
+    }, []);
+
     return (
     <>
-        Products List
+        <h3>Products List</h3>
+        <ul>
+            {products.map((product) => {
+                return (
+                    <li key={product.id} className="py-4">
+                        <ProductCard product={product} />
+                    </li>
+                )
+            })}
+        </ul>
     </>
     );
   };
