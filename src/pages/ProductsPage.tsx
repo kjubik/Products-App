9c6d528d0aed0afe6b011d203fc196441fda11d6
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-11-25 19:48:56 +0100

    fix: wrap page title in header tage

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -4,7 +4,7 @@ import ProductsList from 'src/components/products/ProductList';
 const ProductsPage = () => {
   return (
     <>
-      Products Page
+      <h2>Products Page</h2>
       <ProductsList />
     </>
   );
