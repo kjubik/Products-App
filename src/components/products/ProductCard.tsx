c9b54b21f42227ac78fe2bd97ab17e0da10d490d
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-11-27 03:12:53 +0100

    feat: add custom styling to ProductCard component

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -9,10 +9,25 @@ const ProductsCard = (props: ProductCardProps) => {
 
     return (
         <>
-            <div>
-                <img src={props.product.imageUrl} alt="product image" className="max-w-sm" />
-                <h4>{props.product.title}</h4>
-                <p>{props.product.description}</p>
+            <div className="flex flex-col gap-4 max-w-sm p-4 rounded">
+                <img src={props.product.imageUrl} alt="product image" 
+                className="max-w-sm rounded" />
+                <div className="flex flex-col gap-2 px-1">
+                    <h4 className="text-3xl font-bold text-slate-900 tracking-tight">
+                        {props.product.title}
+                    </h4>
+                    <p className="text-sm font-regular text-slate-400 tracking-tight">
+                        {props.product.description}
+                    </p>
+                </div>
+                <div className="flex justify-between px-1">
+                    <p className="text-sm font-regular text-slate-600 tracking-tight">
+                        {props.product.creatorUserId}
+                    </p>
+                    <p className="text-sm font-regular text-slate-600 tracking-tight">
+                        {props.product.creationDate.toDate().toLocaleDateString()}
+                    </p>
+                </div>
             </div>
         </>
     );
