681a65a2ac1ae47d50452c3ba2d24acad8060577
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-11-30 03:00:52 +0100

    feat: allow users to publish products to database

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,16 +1,23 @@
 import { useState, useEffect } from "react";
-import Select from "react-select";
+import Select, { ActionMeta, MultiValue } from "react-select";
+import { auth } from "src/App";
 import { getCategoriesAsOptions } from "src/api/categoriesApi";
-import { Category, OptionType } from "src/types";
+import { postProduct } from "src/api/productsApi";
+import { getUsername } from "src/api/usersApi";
+import { OptionType, Product } from "src/types";
+import { Timestamp } from "firebase/firestore";
+import { Comment } from "src/types";
 
 const CreatePost = () => {
 
+    const [newProduct, setNewProduct] = useState<Product>({
+        isDeleted: false,
+        creatorUserId: auth.currentUser?.uid,
+        creatorUsername: '',
+        comments: [] as Comment[],
+    } as Product);
+
     const [descriptionLength, setDescriptionLength] = useState(0);
-
-    const handleDescriptionInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setDescriptionLength(e.target.value.length);
-    }
-
     const [categories, setCategories] = useState<OptionType[]>([]);
 
     useEffect(() => {
@@ -18,35 +25,86 @@ const CreatePost = () => {
             const categoriesFromFirestore = await getCategoriesAsOptions();
             setCategories(categoriesFromFirestore);
         }
+        const fetchUsernameFromFirestore = async () => {
+            if (!auth.currentUser?.uid) return;
+            const username = await getUsername(auth.currentUser?.uid);
+            setNewProduct({
+                ...newProduct,
+                creatorUsername: username
+            })
+        }
 
         fetchCategoriesFromFirestore();
+        fetchUsernameFromFirestore();
     }, []);
+
+    const handleDescriptionInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setDescriptionLength(e.target.value.length);
+        setNewProduct({
+            ...newProduct,
+            description: e.target.value
+        })
+    }
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setNewProduct({
+            ...newProduct,
+            [name]: value
+        })
+    }
+
+    const handleMultiSelectChange = (selectedOptions: MultiValue<OptionType>) => {
+        setNewProduct({
+            ...newProduct,
+            categories: selectedOptions.map(option => option.value)
+        })
+    }
+
+    const handlePublishPost = async () => {
+        if (!newProduct.imageUrl || !newProduct.title || !newProduct.description || !newProduct.categories) {
+            alert("Please fill out all fields");
+            return;
+        }
+
+        console.log(newProduct);
+        const creationDate = new Date();
+        setNewProduct({
+            ...newProduct,
+            creationDate: Timestamp.fromDate(creationDate)
+        })
+
+        await postProduct(newProduct);
+    }
 
     return (
         <>
             <div className='p-4 mx-auto flex flex-col gap-4 max-w-sm rounded'>
-                <input type="url" placeholder="Paste image URL"
+                <input name="imageUrl" onChange={handleInputChange} type="url" placeholder="Paste image URL"
                 className="rounded px-3 py-2 outline outline-1 outline-slate-200
                 focus:outline-2 focus:outline-blue-400"/>
-                <input type="text" placeholder="Product title"
+                <input name="title" onChange={handleInputChange} type="text" placeholder="Product title"
                 className="focus:outline-none bg-inherit text-2xl font-semibold"/>
                 <div className="flex flex-col gap-1">
-                    <textarea onInput={handleDescriptionInput} placeholder="Write a description" maxLength={200}
+                    <textarea name="description" onInput={handleDescriptionInput} placeholder="Write a description" maxLength={200}
                     className="rounded px-3 py-2 outline outline-1 outline-slate-200
                     pb-20 resize-none focus:outline-2 focus:outline-blue-400"/>
                     <div className="min-w-full text-right text-sm text-slate-400">{descriptionLength}/200 characters</div>
                 </div>
-                <input type="text" placeholder="Search for category"
-                className="rounded px-3 py-2 outline outline-1 outline-slate-200
-                focus:outline-2 focus:outline-blue-400"/>
-
                 <Select
                     options={categories}
                     isMulti
                     name="categories"
                     className="basic-multi-select"
                     classNamePrefix="select"
+                    onChange={handleMultiSelectChange}
                 />
+                <button onClick={handlePublishPost} 
+                className="rounded-full bg-blue-500 text-white font-semibold 
+                px-4 py-1 flex items-center justify-around text-lg
+                shadow-md shadow-blue-200 hover:bg-blue-700 mt-4">
+                    Publish
+                </button>
             </div>
         </>
     );
