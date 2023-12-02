7adc27bbfda9e69b7f4cd5bc65f7857984c787e1
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-02 03:26:09 +0100

    feat: replace react-select with html select tag

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,30 +1,30 @@
 import { useState, useEffect } from "react";
-import Select, { ActionMeta, MultiValue } from "react-select";
 import { auth } from "src/App";
-import { getCategoriesAsOptions } from "src/api/categoriesApi";
 import { postProduct } from "src/api/productsApi";
 import { getUsername } from "src/api/usersApi";
-import { OptionType, Product } from "src/types";
-import { Timestamp } from "firebase/firestore";
+import { Category, Product } from "src/types";
+import { Timestamp, serverTimestamp  } from "firebase/firestore";
 import { Comment } from "src/types";
+import { getCategories } from "src/api/categoriesApi";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
+
+    const navigate = useNavigate();
 
     const [newProduct, setNewProduct] = useState<Product>({
         isDeleted: false,
         creatorUserId: auth.currentUser?.uid,
         creatorUsername: '',
         comments: [] as Comment[],
+        creationDate: serverTimestamp(),
+        categories: [] as string[],
     } as Product);
 
     const [descriptionLength, setDescriptionLength] = useState(0);
-    const [categories, setCategories] = useState<OptionType[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]);
 
     useEffect(() => {
-        const fetchCategoriesFromFirestore = async () => {
-            const categoriesFromFirestore = await getCategoriesAsOptions();
-            setCategories(categoriesFromFirestore);
-        }
         const fetchUsernameFromFirestore = async () => {
             if (!auth.currentUser?.uid) return;
             const username = await getUsername(auth.currentUser?.uid);
@@ -33,9 +33,13 @@ const CreatePost = () => {
                 creatorUsername: username
             })
         }
+        const fetchCategoriesFromFirestore = async () => {
+            const fetchedCategories = await getCategories();
+            setCategories(fetchedCategories);
+        }
 
-        fetchCategoriesFromFirestore();
         fetchUsernameFromFirestore();
+        fetchCategoriesFromFirestore();
     }, []);
 
     const handleDescriptionInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -54,51 +58,71 @@ const CreatePost = () => {
         })
     }
 
-    const handleMultiSelectChange = (selectedOptions: MultiValue<OptionType>) => {
-        setNewProduct({
-            ...newProduct,
-            categories: selectedOptions.map(option => option.value)
-        })
+    const handleSelectCategory = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const { value } = e.target;
+        if (!newProduct.categories) {
+            setNewProduct({
+                ...newProduct,
+                categories: [value]
+            })
+        } else {
+            setNewProduct({
+                ...newProduct,
+                categories: [...newProduct.categories, value]
+            })
+        }
+    }
+
+    const validateProductPost = () => {
+        return true;
     }
 
     const handlePublishPost = async () => {
-        if (!newProduct.imageUrl || !newProduct.title || !newProduct.description || !newProduct.categories) {
-            alert("Please fill out all fields");
+        if (!validateProductPost()) {
+            alert('Please fill all the fields');
             return;
         }
-
+            
         console.log(newProduct);
         const creationDate = new Date();
-        setNewProduct({
+        const timestamp = Timestamp.fromDate(creationDate);
+        const updatedProduct = {
             ...newProduct,
-            creationDate: Timestamp.fromDate(creationDate)
-        })
+            creationDate: timestamp
+        };
 
-        await postProduct(newProduct);
+        await postProduct(updatedProduct);
+        navigate('/products');   
     }
 
     return (
         <>
             <div className='p-4 mx-auto flex flex-col gap-4 max-w-sm rounded'>
-                <input name="imageUrl" onChange={handleInputChange} type="url" placeholder="Paste image URL"
+                <input name="imageUrl" type="url"
+                onChange={handleInputChange} placeholder="Paste image URL"
                 className="rounded px-3 py-2 outline outline-1 outline-slate-200
                 focus:outline-2 focus:outline-blue-400"/>
-                <input name="title" onChange={handleInputChange} type="text" placeholder="Product title"
+
+                <input name="title" 
+                onChange={handleInputChange} type="text" placeholder="Product title"
                 className="focus:outline-none bg-inherit text-2xl font-semibold"/>
+
                 <div className="flex flex-col gap-1">
-                    <textarea name="description" onInput={handleDescriptionInput} placeholder="Write a description" maxLength={200}
+                    <textarea name="description"
+                    onInput={handleDescriptionInput} placeholder="Write a description" maxLength={200}
                     className="rounded px-3 py-2 outline outline-1 outline-slate-200
                     pb-20 resize-none focus:outline-2 focus:outline-blue-400"/>
-                    <div className="min-w-full text-right text-sm text-slate-400">{descriptionLength}/200 characters</div>
+
+                    <div className="w-full text-right text-sm text-slate-400">{descriptionLength}/200 characters</div>
                 </div>
-                <Select
-                    options={categories}
-                    isMulti
-                    name="categories"
-                    className="basic-multi-select"
-                    classNamePrefix="select"
-                    onChange={handleMultiSelectChange}
-                />
+
+                <select name="categories" id="categories" placeholder="Select a category" multiple
+                onChange={handleSelectCategory}>
+                    {categories.map((category, index) => (
+                        <option key={index} value={category.name}>{category.name}</option>
+                    ))}
+                </select>
+                
                 <button onClick={handlePublishPost} 
                 className="rounded-full bg-blue-500 text-white font-semibold 
                 px-4 py-1 flex items-center justify-around text-lg
