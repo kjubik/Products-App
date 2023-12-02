d57104f7cb8ba08726b478209495fdad006c6f08
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-02 03:25:36 +0100

    feat: rearrange tags order

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -8,9 +8,19 @@ const ProductsCard = (props: ProductCardProps) => {
 
     return (
         <>
-            <div className="flex flex-col gap-4 max-w-sm p-4 rounded-lg bg-slate-50">
+            <div className="flex flex-col gap-4 max-w-sm p-4 rounded-lg bg-slate-50 
+            outline outline-1 outline-slate-200">
+                {/* {if the user is an admin render this} */}
+                <div className="flex w-full justify-between gap-4 px-1">
+                    <p className="">
+                        {props.product.creatorUsername}
+                    </p>
+                    <button>Options</button>
+                </div>
+
                 <img src={props.product.imageUrl} alt="product image" 
                 className="max-w-sm rounded" />
+
                 <div className="flex flex-col gap-2 px-1">
                     <h4 className="text-3xl font-bold text-slate-900 tracking-tight">
                         {props.product.title}
@@ -19,15 +29,10 @@ const ProductsCard = (props: ProductCardProps) => {
                         {props.product.description}
                     </p>
                 </div>
-                <div className="flex justify-between px-1
-                text-sm font-regular text-slate-500">
-                    <p className="">
-                        {props.product.creatorUsername}
-                    </p>
-                    <p className="">
-                        {props.product.creationDate.toDate().toLocaleString('en-GB', { day: '2-digit', month: 'short', year: 'numeric', hour: '2-digit', minute: '2-digit' })}
-                    </p>
-                </div>
+
+                <p className="text-slate-400">
+                    {props.product.creationDate.toDate().toLocaleString('en-GB', { day: '2-digit', month: 'short', year: 'numeric', hour: '2-digit', minute: '2-digit' })}
+                </p>
                 <input type="text" placeholder="Write a comment" className="rounded-full outline outline-1 outline-slate-300 
                 px-4 py-2 w-full placeholder-slate-400 bg-inherit text-slate-900
                 focus:outline focus:outline-2 focus:outline-blue-400"/>
