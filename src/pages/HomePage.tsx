607773a813289557b10d40a847c8939970ce41f3
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-06 22:44:06 +0100

    fix: remove temporary button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,9 @@
-import { postProduct } from "src/services/productsServices";
-import { exampleData } from "src/services/exampleData";
 
 
 const HomePage = () => {
 
-  const addData = async () => {
-    exampleData.forEach(async (product) => {
-      await postProduct(product);
-    });
-  }
-
   return (
     <>
-      <button onClick={addData}>Add data</button>
     </>
   );
 };
