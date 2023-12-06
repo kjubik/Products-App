1c001515e90aaee64c879f5057b75f3b89c80f18
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-06 11:07:55 +0100

    feat: add example data for creating temp posts

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
+import { postProduct } from "src/services/productsServices";
+import { exampleData } from "src/services/exampleData";
 
 
 const HomePage = () => {
 
+  const addData = async () => {
+    exampleData.forEach(async (product) => {
+      await postProduct(product);
+    });
+  }
+
   return (
     <>
-    
+      <button onClick={addData}>Add data</button>
     </>
   );
 };
