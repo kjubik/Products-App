7437c6053ae2744af8b6bde0e700482fc41ce087
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-08 13:25:32 +0100

    fix: change anchor tags to Link

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,6 +2,7 @@ import GoogleAuthButton from "../authentication/GoogleAuthButton";
 import SignOutButton from "../authentication/SignOutButton";
 import { useState, useEffect } from "react";
 import { onAuthStateChanged, getAuth } from "firebase/auth";
+import { Link } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -24,10 +25,10 @@ const Navbar = () => {
     return (
         <>
         <nav className="w-full bg-slate-200 flex items-center align-center justify-between px-8 py-4">
-            <h1 className="font-bold"><a href="/">produkty app</a></h1>
+            <h1 className="font-bold"><Link to="/">produkty app</Link></h1>
             <ul className="flex gap-8">
-                <li><a href="/products" className="text-blue-500 hover:text-blue-700 font-semibold">Products</a></li>
-                <li><a href="/profile" className="text-blue-500 hover:text-blue-700 font-semibold">Profile</a></li>
+                <li><Link to="/products" className="text-blue-500 hover:text-blue-700 font-semibold">Products</Link></li>
+                <li><Link to="/profile" className="text-blue-500 hover:text-blue-700 font-semibold">Profile</Link></li>
             </ul>
             {isAuthed ? <SignOutButton /> : <GoogleAuthButton />}
         </nav>
