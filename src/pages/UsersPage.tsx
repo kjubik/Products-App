be579452eccf466b72131dffb8d2878ba35b0d8a
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-24 04:08:33 +0100

    implement getAllUsers & UserRow

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,11 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getUser } from "src/firebase/services/users";
+import { getUser, getAllUsers } from "src/firebase/services/users";
 import { getAuth } from "firebase/auth";
+import { User } from "src/types/User";
+import UserRow from "src/components/users/UserRow";
 
 
 const UsersPage = () => {
     const navigate = useNavigate();
+
+    const [users, setUsers] = useState<User[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const checkUserIsAdmin = async () => {
@@ -19,12 +24,35 @@ const UsersPage = () => {
             }
         };
 
+        const getUsersFromFirestore = async () => {
+            if (!loading) return;
+            const users = await getAllUsers();
+            setUsers(users);
+            setLoading(false);
+        }
+
         checkUserIsAdmin();
+        getUsersFromFirestore();
     }, []);
 
     return (
         <div>
             <h1>Users</h1>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Admin</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user) => (
+                        <UserRow key={user.id} user={user} />
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 };
