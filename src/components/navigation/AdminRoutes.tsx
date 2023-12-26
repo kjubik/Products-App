4ba0d7bc08b3e3fc7be08c72ba519b45d63fdcb9
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-26 16:49:46 +0100

    feat: rework file to check if user is admin

--- src/components/navigation/AdminRoutes.tsx
+++ src/components/navigation/AdminRoutes.tsx
@@ -1,25 +1,38 @@
 import { useState, useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAdminStatus } from "src/firebase/getAdminStatus";
 
 const AdminRoutes = () => {
 
-    const [isAuthed, setIsAuthed] = useState<boolean | null>(null);
+    const [isAdmin, setIsAdmin] = useState<boolean | null>(null);
 
     useEffect(() => {
+        const checkIfAdmin = async () => {
+            getAdminStatus().then((adminStatus) => {
+                if (adminStatus) {
+                    console.log('allowing access to admin routes');
+                    setIsAdmin(true);
+                } else {
+                    console.log('denied access to admin routes');
+                    setIsAdmin(false);
+                }
+            });
+        }
+
         const auth = getAuth();
         onAuthStateChanged(auth, (firebaseUser) => {
             if (firebaseUser) {
                 console.log('allowing access to protected routes');
-                setIsAuthed(true);
+                checkIfAdmin();
             } else {
                 console.log('denied access to protected routes');
-                setIsAuthed(false);
+                setIsAdmin(false);
             }
         });
     }, []);
 
-    return isAuthed === null ? <>Loading page...</> : isAuthed ? <Outlet /> : <Navigate to="/" />;
+    return isAdmin === null ? <>Loading page...</> : isAdmin ? <Outlet /> : <Navigate to="/" />;
 }
 
 export default AdminRoutes;
