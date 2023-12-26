29fb66061ba9aafaad1e5eb4716da29733748687
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-26 20:11:30 +0100

    feat: get user docs from firestore in UsersPage

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,13 +1,28 @@
 import UsersList from "src/components/users/UsersList";
-
+import { useEffect, useState } from "react";
+import { User } from "src/firebase/types/User";
+import { getAllUsers } from "src/firebase/services/users";
 
 
 const UsersPage = () => {
     
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const data = await getAllUsers();
+            setUsers(data);
+            setIsLoaded(true);
+        };
+
+        fetchUsers();
+        console.log(users);
+    }, []);
     
     return(<>
         <h1>Users Page</h1>
-        <UsersList />
+        {isLoaded ? <UsersList /> : <p>Loading users...</p>}
     </>)
 }
 
