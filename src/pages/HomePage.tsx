7aebf71a7e389225e89c166d539f0ad5800d7e0b
Wojciech Kubicki <wojt.kubicki@gmail.com>
2023-12-27 11:02:10 +0100

    feat: get react-tailwindcss-select working on Home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,35 @@
+import Select from "react-tailwindcss-select";
+import { useState } from "react";
+import { SelectValue } from "react-tailwindcss-select/dist/components/type";
 
+const options = [
+    { value: "fox", label: "🦊 Fox" },
+    { value: "Butterfly", label: "🦋 Butterfly" },
+    { value: "Honeybee", label: "🐝 Honeybee" }
+];
 
 const HomePage = () => {
+    const [animal, setAnimal] = useState<SelectValue>([]);
 
-  return (
-    <>
-    </>
-  );
+    const handleChange = (value: SelectValue) => {
+            console.log("value:", value);
+            setAnimal(value);
+    };
+
+    return (
+        <>
+            <Select 
+                    value={animal}
+                    onChange={handleChange}
+                    options={options} 
+                    isMultiple={true}
+                    primaryColor={'blue'}
+            />
+            {animal && Array.isArray(animal) && animal.map((item) => (
+                    <div key={item.value}>{item.label}</div>
+            ))}
+        </>
+    );
 };
 
 export default HomePage;
