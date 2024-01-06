ce8d8c264c604e9fca25630649e7559f70229801
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-01-06 03:06:30 +0100

    ref: clean up commented out field

--- src/firebase/types/Product.ts
+++ src/firebase/types/Product.ts
@@ -4,7 +4,6 @@ import { FieldValue, Timestamp } from "firebase/firestore"
 export type Product = {
     id?: string,
     categories: string[],
-    // comments?: Comment[],
     creationDate: Timestamp | FieldValue,
     creatorDisplayName: string,
     creatorUserId: string,
