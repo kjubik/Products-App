34b3290bbb0eef1dcdc677e45394c12d5d8b1ba7
Wojciech Kubicki <wojt.kubicki@gmail.com>
2024-01-15 16:30:58 +0100

    feat: add content to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,25 @@
+import { Link } from 'react-router-dom';
 
 
 const HomePage = () => {
 
     return (
-        <>
-            <h1>Home Page</h1>
-        </>
+      <div
+      className="flex flex-col gap-4 items-center justify-center p-6"
+      >
+          <h1 className="text-4xl font-bold text-gray-800 mb-8">
+            ProductsApp
+          </h1>
+          <p>An account is required to use the app.</p>
+          <div className="flex gap-4">
+            <Link to='/create-account' className="rounded-full bg-blue-500 hover:bg-blue-700 px-4 py-1 text-white font-semibold">
+                Create Account
+            </Link>
+            <Link to='/sign-in' className="rounded-full bg-slate-400 hover:bg-slate-600 px-4 py-1 text-white font-semibold">
+                Sign In
+            </Link>
+          </div>
+      </div>  
     );
 };
 
